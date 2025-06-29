701693562670a090718d4724f70204da7aa001b1
jiazhigang <jiazhigang_888@163.com>
2025-06-29 12:12:16 +0800

    feat: 优化请求和路由拦截器，调整代码格式，提升可读性和一致性

--- src/interceptors/request.ts
+++ src/interceptors/request.ts
@@ -1,16 +1,16 @@
-import qs from 'qs'
-import { useUserStore } from '@/store'
-import { getEnvBaseUrl } from '@/utils'
-import { platform } from '@/utils/platform'
+import qs from 'qs';
+import { useUserStore } from '@/store';
+import { getEnvBaseUrl } from '@/utils';
+import { platform } from '@/utils/platform';
 
 export type CustomRequestOptions = UniApp.RequestOptions & {
-  query?: Record<string, any>
+  query?: Record<string, any>;
   /** 出错时是否隐藏错误提示 */
-  hideErrorToast?: boolean
-} & IUniUploadFileOptions // 添加uni.uploadFile参数类型
+  hideErrorToast?: boolean;
+} & IUniUploadFileOptions; // 添加uni.uploadFile参数类型
 
 // 请求基准地址
-const baseUrl = getEnvBaseUrl()
+const baseUrl = getEnvBaseUrl();
 
 // 拦截器配置
 const httpInterceptor = {
@@ -18,12 +18,12 @@ const httpInterceptor = {
   invoke(options: CustomRequestOptions) {
     // 接口请求支持通过 query 参数配置 queryString
     if (options.query) {
-      const queryStr = qs.stringify(options.query)
+      const queryStr = qs.stringify(options.query);
       if (options.url.includes('?')) {
-        options.url += `&${queryStr}`
+        options.url += `&${queryStr}`;
       }
       else {
-        options.url += `?${queryStr}`
+        options.url += `?${queryStr}`;
       }
     }
     // 非 http 开头需拼接地址
@@ -31,39 +31,39 @@ const httpInterceptor = {
       // #ifdef H5
       if (JSON.parse(import.meta.env.VITE_APP_PROXY)) {
         // 自动拼接代理前缀
-        options.url = import.meta.env.VITE_APP_PROXY_PREFIX + options.url
+        options.url = import.meta.env.VITE_APP_PROXY_PREFIX + options.url;
       }
       else {
-        options.url = baseUrl + options.url
+        options.url = baseUrl + options.url;
       }
       // #endif
       // 非H5正常拼接
       // #ifndef H5
-      options.url = baseUrl + options.url
+      options.url = baseUrl + options.url;
       // #endif
       // TIPS: 如果需要对接多个后端服务，也可以在这里处理，拼接成所需要的地址
     }
     // 1. 请求超时
-    options.timeout = 10000 // 10s
+    options.timeout = 10000; // 10s
     // 2. （可选）添加小程序端请求头标识
     options.header = {
       platform, // 可选，与 uniapp 定义的平台一致，告诉后台来源
-      ...options.header,
-    }
+      ...options.header
+    };
     // 3. 添加 token 请求头标识
-    const userStore = useUserStore()
-    const { token } = userStore.userInfo as unknown as IUserInfo
+    const userStore = useUserStore();
+    const token = userStore.token;
     if (token) {
-      options.header.Authorization = `Bearer ${token}`
+      options.header.Authorization = `Bearer ${token}`;
     }
-  },
-}
+  }
+};
 
 export const requestInterceptor = {
   install() {
     // 拦截 request 请求
-    uni.addInterceptor('request', httpInterceptor)
+    uni.addInterceptor('request', httpInterceptor);
     // 拦截 uploadFile 文件上传
-    uni.addInterceptor('uploadFile', httpInterceptor)
-  },
-}
+    uni.addInterceptor('uploadFile', httpInterceptor);
+  }
+};
